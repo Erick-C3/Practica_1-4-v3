421233a4f729b170b5b380ac6ecb1f62c098ec48
Erick <erick.nahuelpaiva.001@gmail.com>
2023-03-25 19:50:47 -0300

    practica 1-4 v3 terminada

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+document.querySelector("body").innerHTML = `
+<details>
+    <summary>Titulo de info importante</summary>
+    <img src="https://juncotic.com/wp-content/uploads/2016/09/animacion4.gif" alt="gif de programacion">
+    <iframe width="600px" height="300px" src="https://es.wikipedia.org/wiki/The_Imitation_Game" frameborder="0"></iframe>
+    <p>During World War II, a mathematician leads a team of cryptanalysts as they work feverishly to break the Germans' notorious Enigma code.</p>
+    <a href="https://www.netflix.com/ar-en/title/70295172">Ver pelicula</a>
+</details>
+`;
